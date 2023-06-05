342fbcf182ea1b9065f73e3866929dfa12c2ec57
Anthony Castillo-Nakamura <116010417+ToekneeCodes@users.noreply.github.com>
2023-06-05 09:36:05 -0500

    testing stuff

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-import selenium
+from selenium import webdriver
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://youtube.com")
+
+print(driver.title)
+driver.quit()
