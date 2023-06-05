0adec004004fd0b34a2884e91e7de8215c80f55b
Anthony Castillo-Nakamura <116010417+ToekneeCodes@users.noreply.github.com>
2023-06-05 13:52:19 -0500

    test update

--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 from selenium import webdriver
-
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+#specify the path to your chromedriver executable
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
 
-driver.get("https://youtube.com")
+#create a Service object using the path to chromeDriver
+service = Service(PATH)
+#driver = webdriver.Chrome(PATH)
+
+#initialize the Chrome Webdriver using the Service object
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://google.com")
 
 print(driver.title)
-driver.quit()
+
+element = driver.find_element(By.ID,"APjFqb")
+element.send_keys("test")
+element.send_keys(Keys.RETURN)
+
+
+time.sleep(15)
+
+#driver.quit()
